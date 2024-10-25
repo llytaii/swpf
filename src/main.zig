b0484a7c27b43d83b10beee687d066011ef0f941
llytaii <llytaii@pm.me>
2024-10-25 21:47:25 +0200

    small refactor

--- src/main.zig
+++ src/main.zig
@@ -3,34 +3,27 @@ const mem = std.mem;
 const os = std.os;
 const assert = std.debug.assert;
 
-fn validateArgs(args: [][:0]u8) !void {
-    if (args.len != 3) {
-        return error.InvalidArgCount;
-    }
+fn validateArgs(args: [][]const u8) !void {
+    if (args.len != 3) return error.InvalidArgCount;
 
     const stat1 = try std.fs.cwd().statFile(args[1]);
-    if (stat1.kind != .file) {
-        return error.Arg1IsNotAFile;
-    }
+    if (stat1.kind != .file) return error.Arg1IsNotAFile;
 
     const stat2 = try std.fs.cwd().statFile(args[2]);
-    if (stat2.kind != .file) {
-        return error.Arg2IsNotAFile;
-    }
+    if (stat2.kind != .file) return error.Arg2IsNotAFile;
 
-    if (mem.eql(u8, args[1], args[2])) {
-        return error.ArgsAreTheSameFile;
-    }
+    if (mem.eql(u8, args[1], args[2])) return error.ArgsAreTheSameFileName;
 }
 
 fn printHelpTo(writer: anytype) !void {
     try writer.print("usage: swpf <file1> <file2>\n", .{});
     try writer.print("description: swaps the given files\n", .{});
+    try writer.print("note: files must be different and reside in the current working directory.\n", .{});
 }
 
 fn swapFiles(file1: []const u8, file2: []const u8) !void {
-    const tmp_file = "very_long_and_stupid_random_name1224123huizilopochtli";
-    comptime assert(tmp_file.len < 256);
+    const tmp_file = "very_long_and_stupid_random_name1224123huizilopochtli_1212121212";
+    comptime assert(tmp_file.len < 250); // 256 chars is the file size limit on linux
 
     assert(!mem.eql(u8, file1, tmp_file));
     assert(!mem.eql(u8, file2, tmp_file));
@@ -67,6 +60,6 @@ pub fn main() !void {
 
     // swapping
     swapFiles(args[1], args[2]) catch |err| {
-        try stderr.print("{}: manual intervention necessary!\n", .{err});
+        try stderr.print("error {} while swapping, manual intervention necessary!\n", .{err});
     };
 }
